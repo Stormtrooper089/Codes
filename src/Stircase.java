11de4042c46f428dcc15e52f6815fc387662208b
ashish.unhale <ashish.unhale@ril.com>
2022-11-11 11:40:10 +0530

    staircase find n ways

---
+++ src/Stircase.java
@@ -0,0 +1,17 @@
+public class Stircase {
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(findWays(n));
+    }
+
+    private static int findWays(int n) {
+        if(n < 0){
+            return 0;
+        }
+        if(n == 0 ){
+            return 1;
+        }
+        return Math.max(1+findWays(n-1),1+findWays(n-2));
+    }
+
+}
