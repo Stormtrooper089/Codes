31b308defd56e903938aefca6625d8bcfbfc553a
ashish.unhale <ashish.unhale@ril.com>
2022-11-15 23:38:25 +0530

    Jumps

--- src/MedianOfSortedArray.java
+++ src/MedianOfSortedArray.java
@@ -13,39 +13,21 @@ public class MedianOfSortedArray {
         int r2 = 0;
         int a = 0;
         int b = 0;
-        double n =l1+l2;
+        int n =l1+l2;
 
-        for(int i=0;i<(n/2);i++){
+        for(int i=0;i<n/2+1;i++){
 
-            int first=0;
-            if(a>=l1){
-                first = Integer.MAX_VALUE;
-            }else{
-                first = Math.min(nums1[a],Integer.MAX_VALUE);
-            }
-
-            int second = 0;
-            if(b>=l2){
-                second = Integer.MAX_VALUE;
-            }else{
-                second = Math.min(nums2[b],Integer.MAX_VALUE);
-            }
-
-            r1=r2;
-            r2 = Math.min(first,second);
-            System.out.println("the value of r1 "  + r1 +  "  the value of r2 is " + r2 +  "  and the value of n " + n);
-            if(first < second){
-                a++;
-            }else{
-                b++;
-            }
+            int t1 = a < nums1.length ? nums1[a] : Integer.MAX_VALUE;
+            int t2 = b < nums2.length ? nums2[b] : Integer.MAX_VALUE;
+            r2=r1;
+            r1 = t1 < t2 ? nums1[a++] : nums2[b++];
         }
 
 
         if(n%2==0){
             System.out.println((r1+r2)/2.0);
         }else{
-            System.out.println(r2);
+            System.out.println(r1);
         }
 
     }
